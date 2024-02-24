024d9c6982b7de043d6a9dedf87cfb5f5d8cf9d1
stapp425 <stapp425@gmail.com>
2024-02-24 16:36:39 -0600

    HTML Stuff
    lmao

---
+++ RH_SPR2024/script.js
@@ -0,0 +1,17 @@
+
+
+const theme = () => {
+    if(matchMedia("(prefers-color-scheme: light)").matches) {
+
+    } else if(matchMedia("(prefers-color-scheme: dark)").matches) {
+
+    }
+};
+
+function lightTheme() {
+
+}
+
+function darkTheme() {
+
+}
